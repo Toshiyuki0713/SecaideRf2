e6ab5d9b6db2c4859333a71084aa7439d2951979
Toshiyuki0713 <s14051ti@sfc.keio.ac.jp>
2017-11-05 13:35:35 +0900

    update README

---
+++ app/src/main/java/com/dotinstall/secaiderf2/Constants.java
@@ -0,0 +1,10 @@
+package com.dotinstall.secaiderf2;
+
+/**
+ * Created by gosho on 2017/10/22.
+ */
+
+public class Constants {
+    // Set this value to your Channel ID
+    public static final String CHANNEL_ID = "1541292924";
+}
--- app/src/main/java/com/dotinstall/secaiderf2/MainActivity.java
+++ app/src/main/java/com/dotinstall/secaiderf2/MainActivity.java
@@ -1,9 +1,22 @@
 package com.dotinstall.secaiderf2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+
+import com.linecorp.linesdk.auth.LineLoginApi;
+import com.linecorp.linesdk.auth.LineLoginResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -11,36 +24,78 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static android.media.audiofx.AudioEffect.SUCCESS;
+import static okhttp3.internal.http2.ErrorCode.CANCEL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://travossbot.herokuapp.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        final TextView loginButton = (TextView) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
 
-        GitHubService service = retrofit.create(GitHubService.class);
+            public void onClick(View v) {
 
+                try {
+                    // App to App Login
+                    Intent LoginIntent = LineLoginApi.getLoginIntent(v.getContext(), Constants.CHANNEL_ID);
+                    startActivityForResult(LoginIntent, REQUEST_CODE);
 
-
-        Call<BiposiWalk> call = service.getBiposiWalk("user_name");
-
-
-        call.enqueue(new Callback<BiposiWalk>() {
-            @Override
-            public void onResponse(Call<BiposiWalk> call, Response<BiposiWalk> response) {
-
-                ((TextView) findViewById(R.id.textview)).setText("受け取った文字列" + response.body());
-
-            }
-
-            @Override
-            public void onFailure(Call<BiposiWalk> call, Throwable t) {
+                } catch (Exception e) {
+                    Log.e("ERROR", e.toString());
+                }
             }
         });
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_CODE) {
+            Log.e("ERROR", "Unsupported Request");
+            return;
+        }
+
+        LineLoginResult result = LineLoginApi.getLoginResultFromIntent(data);
+
+        switch (result.getResponseCode()) {
+
+            case SUCCESS:
+                // Login successful
+
+                SharedPreferences lineData = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = lineData.edit();
+                editor.putString("user_id", result.getLineProfile().getUserId());
+                editor.apply();
+
+                Intent transitionIntent = new Intent(this, PostLoginActivity.class);
+                //transitionIntent.putExtra("display_name", result.getLineProfile().getDisplayName());
+                transitionIntent.putExtra("line_profile", result.getLineProfile());
+                //transitionIntent.putExtra("line_credential", result.getLineCredential());
+                //transitionIntent.putExtra("user_id", result.getLineProfile().getUserId());
+                startActivity(transitionIntent);
+                break;
+
+
+
+
+            case CANCEL:
+                // Login canceled by user
+
+                Log.e("ERROR", "LINE Login Canceled by user!!");
+                break;
+
+            default:
+                Log.e("ERROR", "Login FAILED!");
+                Log.e("ERROR", result.getErrorData().toString());
+        }
+    }
+
+
+
+
 }
